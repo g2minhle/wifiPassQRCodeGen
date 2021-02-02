de667be3f98267f681489a3ce9c0ac78b69e56a8
Minh <hoale@microsoft.com>
2021-02-02 02:05:18 -0800

    Allow to bookmark link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
-import { ChangeEvent, useState } from "react"
+import { useRouter } from "next/dist/client/router";
+import { ChangeEvent, useEffect, useState } from "react"
 import styles from "../styles/Home.module.css"
 
 export default function Home() {
+
+  const router = useRouter();
 
   const [ssid, setSSID] = useState("");
   const [wifiPassword, setWifiPassword] = useState("");
@@ -9,12 +12,17 @@ export default function Home() {
   const data = `WIFI:T:WPA;S:${ssid};P:${wifiPassword};;`;
   const imageUrl = `https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl=${data}`;
 
+  useEffect(() => {
+    setSSID(router.query["ssid"] as string);
+    setWifiPassword(router.query["wifiPassword"] as string);
+  }, [router.query["ssid"], router.query["wifiPassword"]]);
+
   const onSSIDChanged = (event: ChangeEvent<HTMLInputElement>) => {
-    setSSID(event.target.value);
+    // setSSID(event.target.value);
   }
 
   const onWifiPasswordChanged = (event: ChangeEvent<HTMLInputElement>) => {
-    setWifiPassword(event.target.value);
+    // setWifiPassword(event.target.value);
   }
 
   return (
